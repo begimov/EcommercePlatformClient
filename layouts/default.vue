136d6582cd9eda84bf71dfd68b3c8853027cec27
begimov <begimov@gmail.com>
2018-08-19 10:49:56 +0300

    Navigation component created

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
+    <Navigation />
     <nuxt/>
   </div>
 </template>
 
+<script>
+import Navigation from "@/layouts/partials/Navigation.vue";
+
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
