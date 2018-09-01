4c3c4bcb8768aa93b54be8ad1a73259aa30bb76a
begimov <begimov@gmail.com>
2018-09-01 10:22:40 +0300

    product show page created

---
+++ pages/products/_slug.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>{{ product.name }}</h1>
+                <p class="lead">{{ product.description }}</p>
+                <p><span class="badge badge-success">{{ product.price }}</span></p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            product: null
+        }
+    },
+
+    async asyncData ({ params, app }) {
+        let response = await app.$axios.$get(`products/${params.slug}`)
+
+        return {
+            product: response.data
+        }
+    }
+}
+</script>
