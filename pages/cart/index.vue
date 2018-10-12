6922482cac22fc074bff24bce58840799dedbe80
begimov <begimov@gmail.com>
2018-10-12 12:02:22 +0300

    cart UI updates based on isEmpty status

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -5,23 +5,40 @@
                 <h1>Корзина</h1>
             </div>
         </div>
-        <div class="row">
-            <div class="col">
-                <cart-overview />
+        <template v-if="products.length">
+            <div class="row">
+                <div class="col">
+                    <cart-overview />
+                </div>
             </div>
-        </div>
-        <div class="row ">
-            <div class="col text-right">
-                <a href="" class="btn btn-secondary">Оформить</a>
+            <div class="row">
+                <div class="col text-right">
+                    <a href="#" class="btn btn-secondary" :class="{ 'disabled': isEmpty }">Оформить</a>
+                </div>
             </div>
-        </div>
+        </template>
+        <template v-else>
+            <div class="row">
+                <div class="col">
+                    <h3 class="text-muted mb-3">Ваша корзина пуста! :)</h3>
+                    <nuxt-link :to="{ name: 'index' }" class="btn btn-primary">Продолжить покупки</nuxt-link>
+                </div>
+            </div>
+        </template>
     </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import CartOverview from '@/components/cart/CartOverview'
 
 export default {
+    computed: {
+        ...mapGetters({
+            products: 'cart/products',
+            isEmpty: 'cart/empty'
+        })
+    },
     components: {
         CartOverview
     }
