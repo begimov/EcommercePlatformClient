eb56a1733ad27c4f43370b326b59fa223f203901
begimov <begimov@gmail.com>
2018-10-12 07:50:51 +0300

    cart UI updated

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -1,13 +1,30 @@
 <template>
-    <div class="container">
-        <div class="row">
+    <div class="container mt-5">
+        <div class="row mb-4">
             <div class="col">
                 <h1>Корзина</h1>
             </div>
         </div>
         <div class="row">
             <div class="col">
-                <p>Продукты</p>
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                    </tbody>
+                </table>>
             </div>
         </div>
         <div class="row">
